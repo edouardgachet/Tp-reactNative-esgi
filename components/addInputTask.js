a116ede1c5a7f69820d1bd36457f6837c21ed09f
edouardgachet <edouard.gachet@hetic.net>
2021-07-27 18:36:05 +0200

    inital commit

---
+++ components/addInputTask.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'React';
+
+export default function AddInputTask() {
+    const [value, setValue] = useState('');
+
+    const onChangeText = (text) => {
+        setValue(text)
+    }
+
+    return(
+        <input onChangeText={onChangeText}/>
+        <button type="submit"/>
+    )
+}
